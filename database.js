c5d3208787538bddbb8cfa2bf898d8c86c17304f
Yiu Man Chi <ymcatar@gmail.com>
2015-09-29 21:59:33 +0800

    added openshift support

--- database.js
+++ database.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/mcq');
+var url = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://127.0.0.1:27017/';
+
+mongoose.connect(url + 'mcq');
 
 var Paper = mongoose.model('Paper', {
     title: String,
